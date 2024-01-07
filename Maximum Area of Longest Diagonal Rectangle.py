534103271bcef5f410586a5c165d93ea3bd6fdf8
21p31a0422 <21p31a0422@acet.ac.in>
2024-01-07 15:37:50 +0530

    Leetcode weekly contest 397

---
+++ Maximum Area of Longest Diagonal Rectangle.py
@@ -0,0 +1,65 @@
+# Leetcode Weekly Contest 379
+
+# 100170. Maximum Area of Longest Diagonal Rectangle
+# User Accepted:13378
+# User Tried:15062
+# Total Accepted:13985
+# Total Submissions:41909
+# Difficulty:Easy
+# You are given a 2D 0-indexed integer array dimensions.
+
+# For all indices i, 0 <= i < dimensions.length, dimensions[i][0] represents the length and dimensions[i][1] represents the width of the rectangle i.
+
+# Return the area of the rectangle having the longest diagonal. If there are multiple rectangles with the longest diagonal, return the area of the rectangle having the maximum area.
+
+ 
+
+# Example 1:
+
+# Input: dimensions = [[9,3],[8,6]]
+# Output: 48
+# Explanation: 
+# For index = 0, length = 9 and width = 3. Diagonal length = sqrt(9 * 9 + 3 * 3) = sqrt(90) ≈ 9.487.
+# For index = 1, length = 8 and width = 6. Diagonal length = sqrt(8 * 8 + 6 * 6) = sqrt(100) = 10.
+# So, the rectangle at index 1 has a greater diagonal length therefore we return area = 8 * 6 = 48.
+# Example 2:
+
+# Input: dimensions = [[3,4],[4,3]]
+# Output: 12
+# Explanation: Length of diagonal is the same for both which is 5, so maximum area = 12.
+ 
+
+# Constraints:
+
+# 1 <= dimensions.length <= 100
+# dimensions[i].length == 2
+# 1 <= dimensions[i][0], dimensions[i][1] <= 100
+
+
+
+from math import sqrt
+class Solution:
+    def areaOfMaxDiagonal(self, dimensions: List[List[int]]) -> int:
+        
+        def myFun(arr):
+            return sqrt(arr[0] * arr[0] + arr[1] * arr[1])
+        
+        def area(arr):
+            return arr[0] * arr[1]
+        
+        index_array, diag = [0], myFun(dimensions[0])
+        for i in range(1, len(dimensions)):
+            tmp = myFun(dimensions[i])
+            if diag < tmp:
+                diag = tmp
+                index_array = [i]
+            elif diag == tmp:
+                index_array.append(i)
+        
+        if len(index_array) == 1:
+            return dimensions[index_array[0]][0] * dimensions[index_array[0]][1]
+        
+        mx = 0
+        for j in range(len(index_array)):
+            mx = max(mx, area(dimensions[index_array[j]]))
+        return mx
