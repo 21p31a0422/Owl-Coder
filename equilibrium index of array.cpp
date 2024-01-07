e4c813c22a891851e81c15fc2b74376801c084f5
21p31a0422 <21p31a0422@acet.ac.in>
2024-01-07 23:02:50 +0530

    Owl Coder

---
+++ equilibrium index of array.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Algo - Prefix
+
+int equilibriumIndex(int n, vector<int> &arr){
+    int pre = 0;
+    int post = 0;
+    post = accumulate(arr.begin(), arr.end(), post) - arr[0];
+    for(int j = 1; j < n; j++){
+        pre += arr[j - 1];
+        post -= arr[j];
+        if(pre == post){
+            return j;
+        }
+    } 
+    return -1;
+}
+
+int main(){
+    int T;
+    cin >> T;
+    while(T--){
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for(int i = 0; i < n; i++) cin >> arr[i];
+        int result = equilibriumIndex(n, arr);
+        cout << result << "\n";
+    }
+}
