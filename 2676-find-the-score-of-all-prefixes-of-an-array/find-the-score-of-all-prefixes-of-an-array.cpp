7f4d59831227f4e8cd44379f937304f7e91efca7
Black Panther <99874143+21p31a0422@users.noreply.github.com>
2024-01-16 23:23:03 +0530

    Time: 81 ms (80.40%) | Memory: 57.5 MB (28.90%) - LeetSync

--- 2676-find-the-score-of-all-prefixes-of-an-array/find-the-score-of-all-prefixes-of-an-array.cpp
+++ 2676-find-the-score-of-all-prefixes-of-an-array/find-the-score-of-all-prefixes-of-an-array.cpp
@@ -3,10 +3,11 @@ public:
     vector<long long> findPrefixScore(vector<int>& nums) {
         vector<long long> result;
         long long prefix = 0;
-        int mx = 0;
+        int mx = 0, val = 0;
         for (int i = 0; i < nums.size(); i++) {
-            mx = max(mx, nums[i]);
-            prefix += (mx + nums[i]);
+            val = nums[i];
+            mx = max(mx, val);
+            prefix += (mx + val);
             result.push_back(prefix);
         } return result;
     } 
