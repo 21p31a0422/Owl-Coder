11db8bf0a5532647f71a605461fc7ea1addea934
Black Panther <99874143+21p31a0422@users.noreply.github.com>
2024-01-23 22:20:02 +0530

    Time: 510 ms (12.51%) | Memory: 314.1 MB (7.04%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -9,17 +9,15 @@ public:
     } 
 
     int maxLength(vector<string>& arr) {
+        int mx = 0;
         vector<string> comb;
         string seq;
         for (int i = 0; i < (1 << arr.size()); i++) {
             seq = "";
             for (int j = 0; j < arr.size(); j++) {
                 if (i & (1 << j)) seq += arr[j];
-            } comb.push_back(seq);
+            } if (unique(seq)) mx = (seq.size() > mx) ? seq.size() : mx;
         }
-        int mx = 0;
-        for(int i = 0; i < comb.size(); i++){
-            if (unique(comb[i])) mx = (comb[i].size() > mx) ? comb[i].size() : mx;
-        } return mx;
+        return mx;
     } 
 };
