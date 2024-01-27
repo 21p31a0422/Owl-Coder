81f62bc2939da2bc304dbc039b43ff52c4856663
Black Panther <99874143+21p31a0422@users.noreply.github.com>
2024-01-27 22:24:28 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (7.10%) - LeetSync

---
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if (head->next == NULL) return NULL;
+        ListNode* p1 = head;
+        ListNode* p2 = head;
+
+        while (n--) {
+            p2 = p2->next;
+        }
+        if (p2 == NULL) {
+            return p1->next;
+            // return p1;
+        }
+        while (p2->next != NULL ) {
+            p1 = p1->next; 
+            p2 = p2->next;
+        }
+        // p1->next = (p2->next == NULL && p1->next != NULL) ? NULL : p1->next->next;
+        p1->next = p1->next->next;
+        // p2 = NULL;
+        return head;
+    }
+};
