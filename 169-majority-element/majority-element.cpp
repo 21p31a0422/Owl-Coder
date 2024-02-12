098dd4f29df87b729b7fc682903dfac0487c86fb
Black Panther <99874143+21p31a0422@users.noreply.github.com>
2024-02-12 20:52:36 +0530

    Time: 4 ms (98.82%) | Memory: 22.7 MB (12.55%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        int ln = (nums.size() / 2), ans = 0;
-        for (int i : nums) { mp[i]++; }
-        for (auto it : mp) {
-            if (it.second > ln) ans = it.first;
-        } return ans;
+        sort(nums.begin(), nums.end());
+        return nums[nums.size() / 2];
     }
 };
