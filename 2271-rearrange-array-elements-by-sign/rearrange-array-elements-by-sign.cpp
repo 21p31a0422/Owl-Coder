c1db242ba37c5d17f9503d80e40305913b909fbe
Black Panther <99874143+21p31a0422@users.noreply.github.com>
2024-02-14 23:33:39 +0530

    Time: 156 ms (54.20%) | Memory: 126.3 MB (50.65%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -2,17 +2,12 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
         vector<int> res(nums.size(), 0);
-        int k = 0;
-        for (int i = 0; i < nums.size(); i++){
-            if (nums[i] > 0) {
-                res[k] = nums[i]; 
-                k += 2;   
-            }
-        } k = nums.size() - 1;
-        for (int i = nums.size() - 1; i > -1; i--) {
-            if (nums[i] < 0) {
-                res[k] = nums[i];
-                k -= 2;
+        int i = 0, j = 1;
+        for (int k = 0; k < nums.size(); k++){
+            if (nums[k] > 0) {
+                res[i] = nums[k]; i += 2;   
+            } else {
+                res[j] = nums[k]; j += 2;
             }
         } 
         return res;
