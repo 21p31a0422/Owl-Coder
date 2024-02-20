129068b69b20be09f2279a1228a74c778a34ab3b
Black Panther <99874143+21p31a0422@users.noreply.github.com>
2024-02-20 15:31:53 +0530

    Time: 14 ms (63.35%) | Memory: 20.4 MB (18.88%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int numTotal = 0;
-        for(int i = 0; i < nums.size(); i++) {
+        int numTotal = 0, ln = nums.size();
+        for(int i = 0; i < ln; i++) {
             numTotal += nums[i];
-        } return (((nums.size() * (nums.size() + 1)) / 2) - numTotal);
+        } return (((ln * (ln + 1)) / 2) - numTotal);
     }
 };
