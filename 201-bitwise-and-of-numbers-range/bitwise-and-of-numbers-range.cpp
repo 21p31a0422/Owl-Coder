c02944d2826a8cd5bede1152e7b0581d1f97d711
Black Panther <99874143+21p31a0422@users.noreply.github.com>
2024-02-21 23:28:59 +0530

    Time: 8 ms (53.91%) | Memory: 8.8 MB (46.79%) - LeetSync

--- 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
@@ -3,6 +3,6 @@ public:
     int rangeBitwiseAnd(int left, int right) {
         while(right > left){
             right = right & (right - 1);
-        } return right;
+        }return right;
     }
 };
