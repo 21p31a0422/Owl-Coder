d11f6e8eb394cf4864174a37f7c63e336d5513f0
Black Panther <99874143+21p31a0422@users.noreply.github.com>
2024-02-22 23:29:27 +0530

    Time: 64 ms (64.93%) | Memory: 61.2 MB (49.86%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i = 0, j = height.size() - 1, water = INT_MIN;
+        while(i < j){
+            water = max(water, ((j - i) * min(height[i], height[j])));
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        // water = max(water, ((j - i) * min(height[i], height[j])));
+        return water;
+    }
+};
